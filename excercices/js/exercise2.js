53e5ba3d5498e2804e55e03f0996831f9b96ae9f
oliver7do <afristreaming@gmail.com>
2023-06-01 16:27:06 +0200

    3 commit

--- excercices/js/exercise2.js
+++ excercices/js/exercise2.js
@@ -1,9 +1,46 @@
-var nb1 = prompt("");
-var nb2 = 7;
-nb1 = parseInt(nb1);
-var nb3 = nb1 % nb2; 
-var resultat = nb1 % nb2 + nb3;
-nb3 += nb2;
-console.log(resultat);
-document.write(resultat);
-alert(resultat);
+// var nb1 = prompt("");
+// var nb2 = 7;
+// nb1 = parseInt(nb1);
+// var nb3 = nb1 % nb2;
+// var resultat = nb1 % nb2 + nb3;
+// nb3 += nb2;
+// console.log(resultat);
+// document.write(resultat);
+// alert(resultat);
+// var num1 = 2;
+// var num2 = 5;
+// if ((num1 + num2)==7) {
+//     console.log("le résultat est correct");
+// }
+// if (!(num1 + num2)!=7) {
+//     console.log("le résultat n'est pas correct");
+// }
+// var monCompte = 1000;
+// var temps = 0;
+// while(monCompte < 2000) {
+
+//     monCompte += 50;
+//     temps++;
+// }
+// document.write("<h3>Sous sur mon compte : " + monCompte + "</h3>");
+// document.write("<h3>Jusqu'à 2000€, il me faut : " + temps + " mois</h3>");
+
+var fruits =["Apple", "Banana"];
+console.log('Tableau fruits initial : ' + fruits);
+//Ajouter à la fin du tableau
+var newLength = fruits.push('Orange', 'Citron');
+
+
+console.log('Tableau fruits ajout des deux éléments : ' + fruits);
+console.log(typeof fruits);
+console.log(typeof newLength);
+console.log(newLength);
+
+var lastElement = fruits.pop();
+
+console.log('Tableau fruits après avoir supprimé le dernier élement : ' + fruits);
+console.log('La taille finale du tableau : ' + fruits.length);
+console.log(typeof lastElement);
+console.log('La valeur du dernier élement supprimé : ' + lastElement);
+
+var first = fruits.shift(); 
---
+++ initiation/js/06.js
@@ -0,0 +1,79 @@
+// alert('test');
+
+// L'opérateur de comparaison == signifie 'egal à'
+// il permet de vérifier que les VALEURS de 2 variables sont identiques
+var nb1 = 123;
+var nb2 = "123";
+console.log(nb1 == nb2);// retourne TRUE
+
+// l'opérateur de comparaison === signifie 'strictement égal à'
+// il permet de vérifier la VALEUR et le type
+console.log(nb1 === nb2);// retourne FALSE
+
+// -- L'opérateur != signifie 'différent de ... en VALEUR'
+console.log(nb1 != nb2);// retourne FALSE
+
+// -- L'opérateur !== signifie 'strictement différent de ... en VALEUR et en TYPE'
+console.log(nb1 !== nb2);// retourne TRUE
+
+/* -------------------------------
+            EXERCICE :
+J'arrive sur un Espace Sécurisé au moyen du prénom et de l'âge.
+Je doit saisir mon prénom et mon age pour être authentifié sur le site (les infos sont en BDD, ici dans mes variables prenom et age).
+En cas d'échec une alerte m'informe du problème.
+Si tout se passe bien, un message de bienvenue m'accueille.
+-------------------------------- */
+var prenom, monAge;
+prenom = "Mila";
+monAge = 999;
+
+// 1 -- Demander son prénom à l'utilisateur avec un prompt
+var prenomLogin = prompt("Quel est votre prénom ?");
+// 2 -- Je vérifie si le prénom saisi (prenomLogin) correspond à celui en base de donnée (prenom)
+if (prenomLogin === prenom) {
+    //alert('test');
+    // 2a. Si tout est ok, je continue la vérification avec l'âge
+    // 2a1. Demande a mon utilisateur son âge via un PROMPT
+    var ageLogin = parseInt(prompt("Votre âge ?"));
+    if (ageLogin === monAge) {
+        alert("Bienvenue " + prenomLogin);
+    } else {
+        // 2a2. Si les AGEs ne correspondent pas, je lance une ALERT.
+        alert("Erreur d'âge");
+    }
+} else {
+    // 2b. Sinon, les prénoms ne correspondent pas, je lance une ALERT.
+    alert("Attention, prénom non reconnu");
+}
+
+
+// var prenom, monAge;
+// prenom = "Mila";
+// monAge = 999;
+// mdp = "bibi";
+
+// var prenomLogin = prompt("Quel est votre prénom ?");
+
+// if (prenomLogin === prenom) {
+//     var ageLogin = parseInt(prompt("Votre âge ?"));
+//     if (ageLogin === monAge) {
+//         var mdpLogin = prompt("Le mot de passe ?")
+//         if (mdpLogin === mdp){
+//             alert("Bienvenue " + prenomLogin);
+//         } else {
+//             alert("Pb de mot de passe !");
+//         }
+//     } else {
+//         alert("Erreur d'âge");
+//     }
+// } else {
+//     alert("Attention, prénom non reconnu");
+// }
+
+
+
+
+
+
+
+//FIN
