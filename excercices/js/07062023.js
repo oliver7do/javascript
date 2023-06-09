e7d83bbde3e62e422fcf3f3f36c5732caf23b8ce
oliver7do <afristreaming@gmail.com>
2023-06-09 11:03:47 +0200

    5 commit

--- excercices/js/07062023.js
+++ excercices/js/07062023.js
@@ -24,27 +24,49 @@
 // }
 
 
+// 08-06-2033
+// function additiondeschiffres(paramnum, paramstring, paramtableau) {
 
-function additiondeschiffres(paramnum, paramstring, paramtableau) {
 
-
-    for (var i = 0; i <= paramnum; i++) {
-        paramArray.push(i);
-    }        
+//     for (var i = 0; i <= paramnum; i++) {
+//         paramArray.push(i);
+//     }        
     
-    var resultat = [paramstring, paramtableau];
-    return resultat;
+//     var resultat = [paramstring, paramtableau];
+//     return resultat;
 
 
+// }
+
+// var num = 15;
+// var chainedecaractere = "le résultat de notre fonction : ";
+// var tab =[];
+
+// var valeurRetour = additiondeschiffres(num, chainedecaractere, tab);
+
+// document.write(valeurretour);
+
+// 09-06-2033 primeiro
+
+// var nombre1 = 5;
+// var nomber2 = 3;
+// var vide;
+// console.log("----------Avant--------")
+// console.log("nombre1 :" + nombre1); 
+// console.log("nombre2 :" + nomber2);
+// vide = nombre1;
+// nombre1 = nomber2;
+// nomber2 = vide;
+// // Tapez votre code ici (sans rien modifier d'autre!)
+// console.log("----------Avant--------")
+// console.log("nombre1 :" + nombre1); //Doit afficher 3
+// console.log("nombre2 :" + nomber2); //Doit afficher 5
+
+
+// 09-06-2033 segundo
+var num = prompt();
+while (num <= 3 || num >= 120 || isNaN(num)) {
+    num = prompt();   
 }
-
-var num = 15;
-var chainedecaractere = "le résultat de notre fonction : ";
-var tab =[];
-
-var valeurRetour = additiondeschiffres(num, chainedecaractere, tab);
-
-document.write(valeurretour);
-
-
-
+var majoriteFR = 18;
+function affichAge();
