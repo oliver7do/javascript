ba9e7711e612d56cc1a8443d39ae3c378a7b4678
oliver7do <afristreaming@gmail.com>
2023-06-13 16:52:15 +0200

    6 commit

---
+++ excercices/js/07_comparaison.js
@@ -0,0 +1,13 @@
+var number1 = (prompt("Saisir un number"));
+var number2 = (prompt("Saisir un number"));
+
+if (number1 > number2) {
+    document.write(number1 + " Est plus grand que " + number2);
+}
+else if (number1 < number2) {
+    document.write(number2 + " Est plus petit que " + number2);
+}
+else if (number1 == number2) {   
+     document.write(number1 + " Est égal à " + number2);
+}
+ 
---
+++ excercices/js/jour_suivant.js
@@ -0,0 +1,30 @@
+// let jour = prompt("Veuillez saisir un jour de la semaine :");
+// // Conversion du jour en minuscules pour faciliter la comparaison
+// jour = jour.toLowerCase();
+// let jourSuivant;
+// // Vérification du jour saisi et attribution du jour suivant
+// if (jour === "lundi") {
+//   jourSuivant = "mardi";
+// } else if (jour === "mardi") {
+//   jourSuivant = "mercredi";
+// } else if (jour === "mercredi") {
+//   jourSuivant = "jeudi";
+// } else if (jour === "jeudi") {
+//   jourSuivant = "vendredi";
+// } else if (jour === "vendredi") {
+//   jourSuivant = "samedi";
+// } else if (jour === "samedi") {
+//   jourSuivant = "dimanche";
+// } else if (jour === "dimanche") {
+//   jourSuivant = "lundi";
+// } else {
+//   // Gestion de l'erreur de saisie
+//   console.log("Jour incorrect !");
+// }
+// // Affichage du jour suivant ou de l'erreur
+// if (jourSuivant) {
+//   console.log("Le jour suivant est : " + jourSuivant);
+// }
+// document.write("Le jour suivant est : " + jourSuivant);
+
+
