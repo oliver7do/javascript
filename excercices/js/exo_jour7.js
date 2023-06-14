f7b18cb16bf1c45b63f255e520e06193bd0e3402
oliver7do <afristreaming@gmail.com>
2023-06-14 16:34:24 +0200

    7 commit

---
+++ excercices/js/exo_jour7.js
@@ -0,0 +1,12 @@
+// var prenom = ['oliver', 'marinet', 'adrient', 'chloe', 'nino', 'allia', 'rose', 'jouleca', 'zoe', 'luka', 'chat noir', 'ladybug'];
+
+// var rand = Math.random();
+// var rand10 = rand * 10;
+// document.write(rand10);
+
+function fillArray(num);
+var addElement = [];
+for (var i = 1; i <= 10; i += 2) {
+    var nb1 = prompt("Saisir de données !");
+    
+}
